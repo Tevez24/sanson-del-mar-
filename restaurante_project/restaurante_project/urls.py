f0d3e3f5182fb14d0e0129b61358ba564c8e52fb
roobertojules <roobertojules@gmail.com>
2025-11-14 16:46:43 +0000

    Simplify base.html to use Tailwind CDN for debugging

--- restaurante_project/restaurante_project/urls.py
+++ restaurante_project/restaurante_project/urls.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('restaurante_app.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
